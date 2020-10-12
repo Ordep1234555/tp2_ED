6110455cf9acab992e2f4cfc943eea9cd310f90e
Ordep1234555 <48034949+Ordep1234555@users.noreply.github.com>
2020-10-12 11:38:05 -0300

    Create main.cc

---
+++ pedro_mucci/src_ordenacao_imperador_algoritmo2/main.cc
@@ -0,0 +1,20 @@
+#include <cstdio>
+#include <iostream>
+#include "string.h"
+#include "headers/Selecao.hpp"
+
+using namespace std;
+
+int main() {
+  
+  int num_planetas;
+  cin >> num_planetas;
+  int aux;
+  for (aux = 0; aux < num_planetas; aux++)
+    {
+      int nome_p, dist_p, popu_p;
+      cin >> nome_p >> dista_p >> popu_p;
+  // Talvez um vetor?
+    }
+  return 0;
+}
