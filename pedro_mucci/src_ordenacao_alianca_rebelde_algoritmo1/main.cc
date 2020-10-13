a74f5692c3eb5cc1a980c08a688c3cebe044fc47
Ordep1234555 <48034949+Ordep1234555@users.noreply.github.com>
2020-10-13 15:48:22 -0300

    Versão funcional

--- pedro_mucci/src_ordenacao_alianca_rebelde_algoritmo1/main.cc
+++ pedro_mucci/src_ordenacao_alianca_rebelde_algoritmo1/main.cc
@@ -8,11 +8,11 @@ string nome_p[2000000];
 int dados_p[2000000][2];
 
 void quicksort(int inicio, int fim){
-	int i, j, pivo, aux[1][2];
-  string aux_p;
-	i = inicio;
-	j = fim-1;
-	pivo = dados_p[(inicio + fim)/2][0];
+int i, j, pivo, aux[1][2];
+string aux_p;
+i = inicio;
+j = fim-1;
+pivo = dados_p[(inicio + fim)/2][0];
 	while(i <= j){
 		while(dados_p[i][0] < pivo && i < fim){
 			i++;
@@ -22,14 +22,14 @@ void quicksort(int inicio, int fim){
 		}
 		if(i <= j){
 			aux[0][0] = dados_p[i][0];
-      aux[0][1] = dados_p[i][1];
-      aux_p = nome_p[i];
-      dados_p[i][0] = dados_p[j][0];
-      dados_p[i][1] = dados_p[j][1];
-      nome_p[i] = nome_p[j];
-      dados_p[j][0] = aux[0][0];
-      dados_p[j][1] = aux[0][1];
-      nome_p[j] = aux_p;
+			aux[0][1] = dados_p[i][1];
+			aux_p = nome_p[i];
+			dados_p[i][0] = dados_p[j][0];
+			dados_p[i][1] = dados_p[j][1];
+			nome_p[i] = nome_p[j];
+			dados_p[j][0] = aux[0][0];
+			dados_p[j][1] = aux[0][1];
+			nome_p[j] = aux_p;
 			i++;
 			j--;
 		}
@@ -43,11 +43,11 @@ void quicksort(int inicio, int fim){
 //-------------------------------------------------------------------------------------
 
 void quicksort2(int inicio, int fim){
-	int i, j, pivo, aux[1][2];
-  string aux_p;
-	i = inicio;
-	j = fim-1;
-	pivo = dados_p[(inicio + fim)/2][1];
+int i, j, pivo, aux[1][2];
+string aux_p;
+i = inicio;
+j = fim-1;
+pivo = dados_p[(inicio + fim)/2][1];
 	while(i <= j){
 		while(dados_p[i][1] < pivo && i < fim){
 			i++;
@@ -57,14 +57,14 @@ void quicksort2(int inicio, int fim){
 		}
 		if(i <= j){
 			aux[0][0] = dados_p[i][0];
-      aux[0][1] = dados_p[i][1];
-      aux_p = nome_p[i];
-      dados_p[i][0] = dados_p[j][0];
-      dados_p[i][1] = dados_p[j][1];
-      nome_p[i] = nome_p[j];
-      dados_p[j][0] = aux[0][0];
-      dados_p[j][1] = aux[0][1];
-      nome_p[j] = aux_p;
+			aux[0][1] = dados_p[i][1];
+			aux_p = nome_p[i];
+			dados_p[i][0] = dados_p[j][0];
+			dados_p[i][1] = dados_p[j][1];
+			nome_p[i] = nome_p[j];
+			dados_p[j][0] = aux[0][0];
+			dados_p[j][1] = aux[0][1];
+			nome_p[j] = aux_p;
 			i++;
 			j--;
 		}
@@ -79,14 +79,33 @@ void quicksort2(int inicio, int fim){
 
 int main() {
   
-  int num_planetas;
-  cin >> num_planetas;
-  int i;
-  for (i = 0; i < num_planetas; i++)
-    {
-      cin >> nome_p[i] >> dados_p[i][0] >> dados_p[i][1];
-    }
-  
+int num_planetas;
+int i,j,k,aux;
+	cin >> num_planetas;
+	for (i = 0; i < num_planetas; i++){
+		cin >> nome_p[i] >> dados_p[i][0] >> dados_p[i][1];
+        }
+	quicksort(0,num_planetas);
+	aux=dados_p[0][0];
+	for(i=1,k=0,j=1; i<num_planetas; i++){
+		if(dados_p[i][0] == aux){
+			j++; //contabiliza quantos planetas tem a mesma distancia
+			if(i= num_planetas-1){ //se forem os planetas até o final dos dados ele arruma até o final
+				quicksort2(k,j+k);
+			}
+		} else {
+			if(j>1){
+				quicksort2(k,k+j);
+			}
+			aux= dados_p[i][0];
+			k=i;
+			j=1;
+		}
+	}
+	cout << endl << "-------------------------------------------------------------------------------------" << endl;
+	for (int i = 0; i < num_planetas; i++) {
+        cout << nome_p[i] + " " + to_string(dados_p[i][0]) + " " + to_string(dados_p[i][1]) << endl;
+    	}
   
   return 0;
 }
